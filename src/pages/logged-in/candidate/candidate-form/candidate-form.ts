1f98c2e76fea8e90bec0784d7b70c439ed8d84a4
Khalid <khalid@bawes.net>
2017-05-05 16:05:19 +0300

    Functional file selection and upload without form assignment

--- src/pages/logged-in/candidate/candidate-form/candidate-form.ts
+++ src/pages/logged-in/candidate/candidate-form/candidate-form.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController, ViewController, LoadingController, AlertController, NavParams } from 'ionic-angular';
+import { Component, Renderer, ElementRef, ViewChild } from '@angular/core';
+import { NavController, Platform, ViewController, LoadingController, AlertController, ActionSheetController, NavParams } from 'ionic-angular';
 // Forms
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomValidator } from '../../../../validators/custom.validator';
 // Providers
+import { AwsService } from '../../../../providers/aws.service';
+import { CameraService } from '../../../../providers/camera.service';
 import { CandidateService } from '../../../../providers/logged-in/candidate.service';
 import { BankService } from '../../../../providers/logged-in/bank.service';
 import { UniversityService } from '../../../../providers/logged-in/university.service';
@@ -17,6 +19,10 @@ import { Candidate } from '../../../../models/candidate';
   templateUrl: 'candidate-form.html'
 })
 export class CandidateFormPage {
+  @ViewChild('fileInput') fileInput:ElementRef;
+
+  public bucketUrl = "https://bawes-public.s3.eu-west-2.amazonaws.com/"; // Used for link generation after upload
+  public uploads = []; // List of uploads to display in this app
 
   public model: Candidate;
   public operation: string;
@@ -40,7 +46,12 @@ export class CandidateFormPage {
     private _fb: FormBuilder,
     private _viewCtrl: ViewController,
     private _loadingCtrl: LoadingController,
-    private _alertCtrl: AlertController
+    private _actionSheetCtrl: ActionSheetController,
+    private _alertCtrl: AlertController,
+    private _awsService: AwsService,
+    private _cameraService: CameraService,
+    private _platform: Platform,
+    private _renderer:Renderer
   ) {
     // Load the passed model if available
     this.model = params.get('model');
@@ -242,12 +253,12 @@ export class CandidateFormPage {
    * All Upload Functionality Below this Line
    */
 
-
   /**
    * Upload Candidate Personal Photo
    */
   photoUpload(){
     console.log("Uploading personal photo");
+    this.uploadBtnClicked();
   }
 
   /**
@@ -255,6 +266,7 @@ export class CandidateFormPage {
    */
   civilFrontUpload(){
     console.log("Uploading front civil photo");
+    this.uploadBtnClicked();
   }
 
   /**
@@ -262,6 +274,125 @@ export class CandidateFormPage {
    */
   civilBackUpload(){
     console.log("Uploading back civil photo");
+    this.uploadBtnClicked();
+  }
+
+  /**
+   * Upload Photo button clicked
+   * - On Native device, load native camera/gallery
+   * - On Browser, trigger a click on the html file input
+   */
+  uploadBtnClicked(){
+    if(this._platform.is("cordova")){
+      // Display action sheet giving user option of camera vs local filesystem.
+      let actionSheet = this._actionSheetCtrl.create({
+        title: "Select image source",
+        buttons: [
+          {
+            text: 'Load from Library',
+            handler: () => {
+              this._cameraService.getImageFromLibrary().then((nativeImageFilePath) => {
+
+                  // Upload and process for progress
+                  this._awsService.uploadNativePath("library", nativeImageFilePath)
+                    .then((uploadObservable) => {
+                      this.processFileUpload(uploadObservable);
+                    })
+                    .catch((err) => {
+                      alert(err);
+                    });
+
+              }, (err) => {
+                  // Error getting picture
+                  alert("Error getting picture from Library: " + JSON.stringify(err));
+                  console.log("Error getting picture from Library: " + JSON.stringify(err));
+              });;
+            }
+          },
+          {
+            text: 'Use Camera',
+            handler: () => {
+              this._cameraService.getImageFromCamera().then((nativeImageFilePath) => {
+
+                  // Upload and process for progress
+                  this._awsService.uploadNativePath("camera", nativeImageFilePath)
+                    .then((uploadObservable) => {
+                      this.processFileUpload(uploadObservable);
+                    })
+                    .catch((err) => {
+                      alert(err);
+                    });
+
+              }, (err) => {
+                  // Error getting picture
+                  alert("Error getting picture from Camera: " + JSON.stringify(err));
+                  console.log("Error getting picture from Camera: " + JSON.stringify(err));
+              });;
+            }
+          }
+        ]
+      });
+      actionSheet.present();
+
+    }else{
+      // Trigger click event on regular HTML file input
+      let event = new MouseEvent('click', {bubbles: true});
+      this._renderer.invokeElementMethod(this.fileInput.nativeElement, 'dispatchEvent', [event]);
+    }
+  }
+
+
+  /**
+   * Upload the selected file through regular HTML file input 
+   * This method will only be called if the target is not a cordova app.
+   */
+  uploadFileViaHtmlFileInput($event){
+    let fileList: FileList = $event.target.files;
+
+    // Check if files available
+    if(fileList.length > 0){
+      let file = fileList.item(0);
+
+      // Upload The File
+      let uploadObservable = this._awsService.uploadFile("browser", file);
+      this.processFileUpload(uploadObservable);
+    }
+  }
+
+  /**
+   * Takes a JS File object for upload to S3
+   */
+  processFileUpload(uploadObservable){
+    // Create Transfer Record for Display 
+    let newUpload = {
+      name: "Preparing file for upload",
+      status: "uploading",
+      loaded: 0,
+      total: 100,
+      link: ''
+    };
+    this.uploads.push(newUpload);
+
+    // Process Upload and Display Progress
+    uploadObservable.subscribe((progress) => {
+      if(progress.loaded &&  progress.loaded != progress.total){
+          newUpload.status = "uploading";
+          newUpload.loaded = progress.loaded;
+          newUpload.total = progress.total;
+      }
+
+      // If Multipart upload (big file), Key with capital "K"
+      if(progress.key || progress.Key){
+        newUpload.name = progress.key? progress.key : progress.Key; 
+        newUpload.link = this.bucketUrl + newUpload.name;
+      }
+
+    }, (err) => {
+      console.log("Error", err);
+      newUpload.status = "error";
+    }, () => {
+      newUpload.status = "complete";
+    });
   }
 
 }
