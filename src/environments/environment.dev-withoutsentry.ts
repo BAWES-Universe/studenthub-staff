e6eaf87ca826a551a56acbf3aaca696cf8ad47dd
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-07-14 16:52:30 +0530

    end points changes done

--- src/environments/environment.dev-withoutsentry.ts
+++ src/environments/environment.dev-withoutsentry.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     envName: 'dev-withoutsentry',
-    apiEndpoint: 'http://payroll-staff.dev.studenthub.co/v1',
+    apiEndpoint: 'http://staff.api.dev.studenthub.co/v1',
     environmentName: 'Dev Server without sentry',
     serviceWorker: true
 }
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@
 export const environment = {
     production: false,
     envName: 'dev',
-    apiEndpoint: 'http://payroll-staff.dev.studenthub.co/v1',
+    apiEndpoint: 'http://staff.api.dev.studenthub.co/v1',
     environmentName: 'Dev Server',
     serviceWorker: true
 };
