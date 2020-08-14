206ba3744ea42076575998d7d50cc799b8d117b4
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-14 12:40:39 +0530

    [Finishes #174313844] loading model created for candidate view page

--- src/app/pages/logged-in/candidate/candidate-view/candidate-view.page.ts
+++ src/app/pages/logged-in/candidate/candidate-view/candidate-view.page.ts
@@ -27,6 +27,8 @@ export class CandidateViewPage implements OnInit {
   public sendingPassword = false;
   public assigning = false;
   public unassinging = false;
+  public loading = false;
+
   constructor(
     public navCtrl: NavController,
     public activatedRoute: ActivatedRoute,
@@ -42,7 +44,6 @@ export class CandidateViewPage implements OnInit {
 
   ngOnInit() {
     const state = window.history.state;
-
     // if (state.model) {
     //   this.candidate = state.model;
     // } else  {
@@ -230,7 +231,9 @@ export class CandidateViewPage implements OnInit {
   }
 
   loadCandidateDetail() {
+    this.loading = true;
     this.candidateService.detail(this.candidate_id).subscribe( response => {
+      this.loading = false;
       this.candidate = response;
     });
   }
