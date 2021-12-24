3a9fa7d01fdca729b2a5f8f51f967dffe86936c7
Krushn Kathrecha <kathrechakrushn@gmail.com>
2021-12-24 17:34:36 +0530

    hotfix

--- src/app/pages/logged-in/story/story-view/story-view.page.ts
+++ src/app/pages/logged-in/story/story-view/story-view.page.ts
@@ -198,6 +198,10 @@ export class StoryViewPage implements OnInit, OnDestroy {
 
         this.story.story_status = status;
 
+        this.eventService.storyStatusUpdated$.next({
+          story: this.story
+        });
+
         // story work started
         if (status == 1) {
             this.authService.story = this.story;
@@ -209,10 +213,6 @@ export class StoryViewPage implements OnInit, OnDestroy {
             this.authService.story = null;
             this.authService.saveInStorage();
         }
-
-        this.eventService.storyStatusUpdated$.next({
-          story: this.story
-        });
 
         // Close the page
         this.loadData();
