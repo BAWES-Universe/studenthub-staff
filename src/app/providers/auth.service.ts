c4a8001dc28ef3b501e16a3e3db7df32b4b9e66f
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2025-01-29 21:32:38 +0530

    hotfix

--- src/app/providers/auth.service.ts
+++ src/app/providers/auth.service.ts
@@ -299,7 +299,7 @@ export class AuthService {
       Currency: this.currency_pref,
       'g-recaptcha-token': token
     });
-    const url = environment.apiEndpoint + this._urlBasicAuth + '?token=' + token;
+    const url = environment.apiEndpoint + this._urlBasicAuth;// + '?token=' + token;
     return this._http.get(url, {
       headers: authHeader,
     }).pipe(
