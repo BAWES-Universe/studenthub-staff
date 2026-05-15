72d41685e1b897daa28fd05a8cd7559d0e61e355
Floyd (LoneStarOracle) <floyd@lonestaroracle.xyz>
2026-05-15 08:39:49 +0000

    Floyd: Fix GitHub issue: [UPGRADE] Replace ng5-slider with @angular
    Task ID: bounty_4452324910_1778834360
    Agent: https://floyd.lonestaroracle.xyz

--- src/app/pages/logged-in/expense/expense-view/expense-view.module.ts
+++ src/app/pages/logged-in/expense/expense-view/expense-view.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { ExpenseViewPageRoutingModule } from './expense-view-routing.module';
+import { ExpenseViewRoutingModule } from './expense-view-routing.module';
 
 import { ExpenseViewPage } from './expense-view.page';
-import { LoadingModalModule } from 'src/app/components/loading-modal/loading-modal.module';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    LoadingModalModule,
-    ExpenseViewPageRoutingModule
+    NgxSliderModule,
+    ExpenseViewRoutingModule
   ],
   declarations: [ExpenseViewPage]
 })
